ebe192333192287e8ee5d5be9e2eb3f24fd2c20b
t1js <97554484+t1js@users.noreply.github.com>
2023-06-27 13:03:48 +0200

    Create form input handles

---
+++ script.js
@@ -0,0 +1,42 @@
+// create array of objects (books)
+
+let books = [
+    {
+    "title": "",
+    "author": "",
+    "chapter": ""
+},
+{
+    "title": "",
+    "author": "",
+    "chapter": ""
+}
+];
+
+let progressForm = document.getElementById("progress-form");
+
+progressForm.addEventListener("submit", submit);
+
+function submit() {
+
+let bookTitle = document.getElementById("book-title");
+let bookAuthor = document.getElementById("book-author");
+let currentChapter = document.getElementById("current-chapter");
+
+  alert("The form was submitted");
+  console.log(
+    `We are adding the ${bookTitle.value} by ${bookAuthor.value} to our database.`
+  );
+
+  //use Object.create() to create new book obj using the inputted details
+
+  const newBook = Object.create(books[0]);
+  newBook.title = bookTitle;
+  newBook.author = bookAuthor;
+  newBook.chapter = currentChapter;
+
+  //use books.push() to add new book obj to the books array
+
+  books.push(newBook);
+
+}
