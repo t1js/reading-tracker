eb2414563f6ceab7c973cddd4ad60fdce656aab1
t1js <97554484+t1js@users.noreply.github.com>
2023-07-03 18:03:45 +0200

    test js file

---
+++ test.js
@@ -0,0 +1,14 @@
+import { remote } from 'webdriverio';
+
+(async () => {
+	const browser = await remote({
+		capabilities:{
+			browserName: 'chrome'
+		}
+	});
+	await browser.url('https://webdriver.io');
+	const title = await browser.getTitle();
+	console.log('Title was: '+ title);
+
+	await browser.deleteSession();
+})().catch((e) => console.error(e));
